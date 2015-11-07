132e5bdec4f8ab7cb46f5edbcc4cdd4b903ff09b
abarathUCLAN <abarath@uclan.ac.uk>
2015-11-07 16:41:22 +0000

    Bower implementation
    Implemented bower client side packet manager into the project and
    changed specific script paths

--- app/app.js
+++ app/app.js
@@ -1,37 +1,28 @@
 'use strict';
 
-var dms = angular.module('dms', ['ngResource']);
+var pdmsys = angular.module('pdmsys', ['ngResource']);
 
-dms.config(function($routeProvider) {
+pdmsys.config(function($routeProvider) {
 
   $routeProvider.
       when('/', {
-        controller: 'RestaurantsController',
-        templateUrl: 'views/restaurants.html'
+        templateUrl: 'index.html'
       }).
-      when('/menu/:restaurantId', {
-        controller: 'MenuController',
-        templateUrl: 'views/menu.html'
+      when('/login', {
+        templateUrl: 'views/login.html'
       }).
-      when('/checkout', {
-        controller: 'CheckoutController',
-        templateUrl: 'views/checkout.html'
+      when('/register', {
+        templateUrl: 'views/register.html'
       }).
-      when('/thank-you', {
-        controller: 'ThankYouController',
-        templateUrl: 'views/thank-you.html'
+      when('/home', {
+        controller: 'HomeController',
+        templateUrl: 'views/home/home.html'
       }).
-      when('/customer', {
-        controller: 'CustomerController',
-        templateUrl: 'views/customer.html'
+      when('/project/:projectId', {
+        controller: '',
+        templateUrl: ''
       }).
-      when('/who-we-are', {
-        templateUrl: 'views/who-we-are.html'
-      }).
-      when('/how-it-works', {
-        templateUrl: 'views/how-it-works.html'
-      }).
-      when('/help', {
-        templateUrl: 'views/help.html'
-      });
+      otherwise({
+        redirectTo: '/'
+      });;
 });
